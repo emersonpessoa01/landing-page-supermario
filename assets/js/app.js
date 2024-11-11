54b591e22d8638d7bf9437f3be6f25def324d32e
emersonpessoa01 <eps011108@gmail.com>
2024-11-11 17:32:12 -0300

    feat: organized JS folders  :rocket:

--- assets/js/app.js
+++ assets/js/app.js
@@ -89,41 +89,11 @@ document.addEventListener("DOMContentLoaded", function () {
     searchAndSlide();
   });
 
-  // Lógica para touch (em dispositivos móveis)
-  searchIcon.addEventListener("touchstart", function () {
-    searchAndSlide();
-  });
+  // // Lógica para touch (em dispositivos móveis)
+  // searchIcon.addEventListener("touchstart", function () {
+  //   searchAndSlide();
+  // });
 });
 
 
 
-/* Menu Hamburger */
-
-const menuBtn = document.getElementById("menu-btn");
-const navbar = document.querySelector(".navbar");
-
-// Função para abrir/fechar o menu
-menuBtn.addEventListener("click", function (event) {
-  event.stopPropagation(); // Impede o evento de propagação para o documento
-  navbar.classList.toggle("show");
-  menuBtn.classList.toggle("fa-times");
-});
-
-// Fechar o menu ao clicar em qualquer link dentro da navbar
-navbar.addEventListener("click", function (event) {
-  if (event.target.tagName === "A") { // Verifica se clicou em um link
-    navbar.classList.remove("show");
-    menuBtn.classList.remove("fa-times");
-  } else if (navbar.classList.contains("show")) { // Se navbar estiver aberta e clicou fora do link
-    navbar.classList.remove("show");
-    menuBtn.classList.remove("fa-times");
-  }
-});
-
-// Fechar o menu ao clicar fora da navbar e do botão de menu
-document.addEventListener("click", function (event) {
-  if (!navbar.contains(event.target) && event.target !== menuBtn) { 
-    navbar.classList.remove("show");
-    menuBtn.classList.remove("fa-times");
-  }
-});
